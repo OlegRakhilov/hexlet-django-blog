20858656c499f57032d3175a09511b7cb49afcf8
Олег <rahilov@gmail.com>
2026-02-09 17:46:07 +0300

    Admin

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ["title", "body"]
+    list_filter = ['created_at']
+
+
+admin.site.register(Article, ArticleAdmin)
