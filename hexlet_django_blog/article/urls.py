5e1afb27ba7217b20a31c25fc457f52f93ed4349
Олег <rahilov@gmail.com>
2026-02-12 17:46:29 +0300

    Edit articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from hexlet_django_blog.article import views
 
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleCreateView,
+    ArticleFormEditView,
+)
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name='articles_index'),
     path("comment/", views.ArticleCommentFormView.as_view(), name='articles_comment_create'),
     path('create/', views.ArticleCreateView.as_view(), name='articles_create'),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", views.ArticleView.as_view(), name='article_detail'),
     path("<str:tags>/<int:article_id>/", views.ArticleView.as_view(), name='article'),
     path("<int:article_id>/comment/", views.ArticleCommentFormView.as_view(), name='articles_comment_create'),
